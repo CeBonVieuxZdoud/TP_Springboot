40205fe97f94f2427a15e1eb16277ce123759c9b
Adrien ROIRAND <adrien.roirand@gmail.com>
2022-11-16 10:41:33 +0100

    truc

--- src/main/java/com/TWICClientApplication.java
+++ src/main/java/com/TWICClientApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TpSpringbootApplication {
+public class TWICClientApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(TpSpringbootApplication.class, args);
+        SpringApplication.run(TWICClientApplication.class, args);
         System.out.println("Application Start");
     }
 
